f6700c9ce71d6ef56ad6980a850f4694ef9d2e58
m-winter <maciekwinter@wp.pl>
2021-01-31 18:02:53 +0100

    news search

--- routes/news.js
+++ routes/news.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const News = require('../models/news')
 
 /* GET home page. */
-router.get('/', (req, res) => {
+router.get('/', (req, res) => {//fixed bug
 
 const search = req.query.search
+
 
   const findNews = News.find({title: new RegExp(search, 'i')});//insensitive search
   findNews.exec((err, data) =>{
