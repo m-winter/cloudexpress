5377e04aa0fe95612bb70683aa3f846665603f8e
m-winter <63043445+m-winter@users.noreply.github.com>
2021-02-11 11:10:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    db: 'mongodb://newsdb98:UlQAdqdaYHyHq6J4oI307PCYHcDuu7QVyDWaInUh18ydxsvodH1LSWLRZ1jEzBNQERVguYrvWMCERXfiWbu5LQ%3D%3D@newsdb98.mongo.cosmos.azure.com:10255/?ssl=true&appName=@newsdb98@',
+    db: '//',
     keySession:['Klucz'],
     maxAgeSession:24 * 60 * 60 * 1000,
-    dbUser: 'newsdb98',
-    dbPass: 'UlQAdqdaYHyHq6J4oI307PCYHcDuu7QVyDWaInUh18ydxsvodH1LSWLRZ1jEzBNQERVguYrvWMCERXfiWbu5LQ==',
+    dbUser: '//',
+    dbPass: '//',
 
 }
